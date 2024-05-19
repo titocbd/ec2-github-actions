64d8f0cc8b5fb200bf2c3bda2bad8732da4d1103
Mahbub Tito <tito@datasoft-bd.com>
2024-05-19 18:26:27 +0600

    2nd Iac code

--- keypair.tf
+++ keypair.tf
@@ -3,8 +3,6 @@ resource "aws_key_pair" "Key-Pair" {
 
   # Name of the Key
   key_name   = "MyKeyFinal"
-
-  # Adding the SSH authorized key !
-  public_key = file("~/.ssh/aws_ssh_key.pub")
+ 
   
  }
